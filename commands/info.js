b20e020544761f1decbfca2e333431529ec4ad44
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-05-30 22:19:04 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
         .addField("bot id:", "427751395123265546")
         .addField("Library:", "discord.js")
         .addField("Owner(s):", "ZezoTheMaster")
-        .addField("support server", "https://discord.gg/DHrC5S")
+        .addField("support server", "https://discord.gg/DjTubAX")
         .setColor('RANDOM')
         .setThumbnail("https://cdn.discordapp.com/avatars/427751395123265546/4a2fcbc57b92e5e6cb951f39fb6b9c8f.png")
     message.channel.send({embed})
