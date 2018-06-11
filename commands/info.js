28620bc0e8e733dcc448a26c5d7b6e75dc778337
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-06-11 10:04:30 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-
+let owners = client.users.get("269132764576481282").tag;
     const embed = new Discord.RichEmbed()
         .setTitle("**Gamers bot** info")
         .setDescription("gamers bot still indev please use .call to give us some ideas")
         .addField("bot version:", "0.5")
         .addField("bot id:", "427751395123265546")
         .addField("Library:", "discord.js")
-        .addField("Owner(s):", "ZezoTheMaster")
+        .addField("Owner(s):", owners)
         .addField("support server", "https://discord.gg/DjTubAX")
         .setColor('RANDOM')
         .setThumbnail("https://cdn.discordapp.com/avatars/427751395123265546/4a2fcbc57b92e5e6cb951f39fb6b9c8f.png")
