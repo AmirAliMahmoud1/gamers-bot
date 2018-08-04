475db44f9ee22eda18645427b867afe0e0d88d22
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-04 12:25:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.commands.set(props.help.name, props);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 console.log(`on ${client.guilds.size} servers`);
-client.user.setActivity('.help | powers' + client.guilds.size + " servers");
+client.user.setActivity('.help | powers ' + client.guilds.size + " servers");
 console.log(client.commands)
 
 });
