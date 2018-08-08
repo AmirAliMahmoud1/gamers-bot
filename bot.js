5bcb931960ffde5015f85eeea02b23a7b3a75c9d
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-08 10:00:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.commands.set(props.help.name, props);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 console.log(`on ${client.guilds.size} servers`);
-client.user.setActivity('.help | Adding new command 1-5 mins'); 
+client.user.setActivity('.help | New .ask command (not completed just test it)'); 
 console.log(client.commands)
 
 });
