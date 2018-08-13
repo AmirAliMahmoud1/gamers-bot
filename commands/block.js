385a65d143a2773b4b5eb9b361996694d80fe703
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-13 21:45:00 +0200

    Update block.js

--- commands/block.js
+++ commands/block.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
+    if (message.author.id !== '269132764576481282' && message.author.id !== '405436409831227404') return message.reply(":x: sorry this command is just for bot owner")
     if (!args[0]) return message.channel.send("Error: invaild ID")
     if (!client.users.get(args[0])) return message.channel.send("Wrong id maybe this user not in mutual servers with this bot")
     let name = client.users.get(args[0]).username
@@ -13,7 +14,7 @@ module.exports.run = async (client, message, args) => {
     fs.writeFile("./commands/block/list.json", JSON.stringify(client.blockList, null, 4), err => {
         if(err) throw err;
 
-        message.channel.send("i blocked him and this is the user name\nthe name :" + name)
+        message.channel.send("i blocked him and this is the user name\nthe name : **" + name + "**")
     });
     
 
