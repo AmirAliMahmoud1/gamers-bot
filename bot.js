1ea6276f43a9b43b2055a536772e51fb04ac88c1
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-14 17:56:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,6 +79,16 @@ setTimeout(() => {
 });
 
 
+var con = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_USER
+});
+
+
+
+
 
 
 //Addong don't touch
