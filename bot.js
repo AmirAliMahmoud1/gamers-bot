eae4140f78da3e0b2edac710e36a07ff1efc2ba3
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-19 10:23:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,8 +71,8 @@ console.log(client.commands)
 client.on('message', message => {
         con.query(`SELECT * FROM prefixes WHERE id = '${message.guild.id}'`, (err, rows) => { 
 
-        if (!rows[0].prefix) prefix = ".";
-        if (rows[0].prefix) prefix = rows[0].prefix;
+        if (rows[0].prefix < 1) prefix = ".";
+        if (rows[0].prefix > 1) prefix = rows[0].prefix;
 
 
     if(!message.content.startsWith(prefix))return;
@@ -126,8 +126,8 @@ client.on("message", message => {
     if (message.author.bot) return;
     con.query(`SELECT * FROM prefixes WHERE id = '${message.guild.id}'`, (err, rows) => { 
 
-        if (!rows[0].prefix) prefix = ".";
-        if (rows[0].prefix) prefix = rows[0].prefix;
+        if (rows[0].prefix < 1) prefix = ".";
+        if (rows[0].prefix > 1) prefix = rows[0].prefix;
 
    if (message.isMentioned(client.user) && message.content === `<@${client.user.id}>`) {
        message.channel.send(`Current server prefix is **${prefix}**\nDo ${prefix}help to get start`);
