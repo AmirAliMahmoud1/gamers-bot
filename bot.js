cf3764cdc7f2ed62e45f09bf77b976b627a6e97f
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-19 12:01:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.commands.set(props.help.name, props);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 console.log(`on ${client.guilds.size} servers`);
-client.user.setActivity('.help | .setprefix added dm the bot if the was any error'); 
+client.user.setActivity('.help | .setprefix added dm the bot if there was any error'); 
 console.log(client.commands)
 
 });
