17dcc1452f67290070d82166b255994a8fa6c909
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-19 11:49:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -127,14 +127,25 @@ setTimeout(() => {
 //Addong don't touch
 
 client.on("message", message => {
+    if(message.channel.type === 'dm') return;
+    con.query(`SELECT * FROM prefixes WHERE id = '${message.guild.id}'`, (err, rows) => {
+        if (!rows[0]) return;
+        if (rows[0]) console.log(rows[0])
+
+        var prefix = '';
+        if (rows[0].prefix < 1) {
+            prefix = ".";
+        } else {
+            prefix = rows[0].prefix;
+        }
     if (message.author.bot) return;
-    let prefix = ".";
+
 
    if (message.isMentioned(client.user) && message.content === `<@${client.user.id}>`) {
        message.channel.send(`Current server prefix is **${prefix}**\nDo ${prefix}help to get start`);
 
    }
-
+    })
 });
 
 //reply and dm chat settings
