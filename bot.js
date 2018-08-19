6dbce2e3b6323a1b6a877ec98a4c649576d9f80e
ZezoCraft <36793243+ZezoCraft@users.noreply.github.com>
2018-08-19 11:53:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.commands.set(props.help.name, props);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 console.log(`on ${client.guilds.size} servers`);
-client.user.setActivity('.help | .mcskin released because of 100 servers <3'); 
+client.user.setActivity('.help | .setprefix added dm the bot if the was any error'); 
 console.log(client.commands)
 
 });
